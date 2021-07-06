baab359a651dbed3497151ce43b63333e2179f5a
theo82 <theoaristi53@gmail.com>
2021-07-06 10:00:30 +0300

    Cocktails - About and Error

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae enim
+        earum, esse neque consequatur ipsa. Iusto minus eum perspiciatis iure
+        vitae? Odio non provident recusandae, aliquid similique corrupti
+        explicabo ullam!
+      </p>
+    </section>
   )
 }
 
