73a648d7bf8a308849d8bfe02dd9632d33e9192b
theo82 <theoaristi53@gmail.com>
2021-07-06 13:09:25 +0300

    Cocktails - Context API Setup

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
 import Cocktail from './Cocktail'
 import Loading from './Loading'
 const CocktailList = () => {
-  return <div></div>
+  const { cocktails, loading } = useGlobalContext()
+  if (loading) {
+    return <Loading />
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no coctails matched your search criteria
+      </h2>
+    )
+  }
+  return (
+    <div>
+      <h1>cocktail list</h1>
+    </div>
+  )
 }
 
 export default CocktailList
